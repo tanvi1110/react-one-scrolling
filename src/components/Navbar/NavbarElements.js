de3f02498aefbb5c14e587ea7473d269c69c513e
Tanvi <tanishaarora1110@gmail.com>
2022-02-12 09:36:01 -0800

    modified

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav `
+       
+       background-color: red;
+       height: auto;
+       max-width: fit-content ;
+       height: 67px; 
+       
+
+`
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    
+    <Nav>
+        <NavbarContainer>
+            <NavLogo>
+                dollar
+            </NavLogo>
+        </NavbarContainer>
+    </Nav>
+    </>
+
+  );
+};
+
+export default Navbar
